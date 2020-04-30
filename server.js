c7cf6755b5df89fad642fe897928f388a07cfe96
Elin Alm <elin95alm@gmail.com>
2020-04-30 12:08:46 +0200

    styling + roomname in chat + enterpassword

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ io.on("connection", (socket) => {
 
     socket.join(data.room, () => {
       // Respond to client that join was success
-      socket.emit("join successful", "success");
+      socket.emit("join successful", data.room);
       handleRoomUsers(data.room, "add");
 
       //Broadcast message to all clients in the room
